0a362956ea11d75b1ec1d54cb24faf011fd944b1
DalockHouston <jeongsik@2ndsyndrome.com>
2025-05-16 16:04:25 +0900

    반복문 응용 추가 학습

--- section01/chapter10.js
+++ section01/chapter10.js
@@ -12,3 +12,20 @@ for (let j = 0; j < 10; j++) {
     if (j % 2 === 0) continue
     console.log(`j는 홀수 : ${j}`);
 }
+
+// 응용
+const arr1 = Array.from({ length: 10}, (v, i) => i);
+console.log(arr1);
+
+console.log('arr1 출력')
+arr1.forEach(ele => {
+    console.log(ele);   
+});
+
+const arr2 = [...Array(10).keys()];
+console.log(arr2);
+
+console.log('arr2 출력')
+arr2.forEach(ele => {
+    if (ele % 2 === 0) console.log(ele);
+});
